43ac232ee0e1511d359267709889fd2ab4f04d5b
Whimsy Elysium Duke <whimsyduke@163.com>
2016-09-22 14:33:23 +0800

    添加项目文件。

---
+++ War3IEEE754Float/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace War3IEEE754Float
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        bool enableEvent = true;
+
+        private void TextBoxHexValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (enableEvent)
+            {
+                try
+                {
+                    if (TextBoxHexValue.Text.Count() == 8)
+                    {
+                        UInt32 hexValue = Convert.ToUInt32(TextBoxHexValue.Text, 16);
+                        //uint signValue = hexValue >> 31;
+                        //uint exponentValue = (hexValue & 0x7F800000) >> 23;
+                        //uint mantissaValue = (hexValue & 0x7FFFFF);
+                        //double floatValue = 0;
+                        //int count = 0;
+                        //uint position = 0x400000;
+                        //while (mantissaValue != 0)
+                        //{
+                        //    count--;
+                        //    if ((mantissaValue & position) != 0)
+                        //    {
+                        //        floatValue += Math.Pow(2, count);
+                        //    }
+                        //    mantissaValue = mantissaValue & (~position);
+                        //    position = position >> 1;
+                        //}
+                        //floatValue = Math.Pow(-1, signValue) * (1 + floatValue) * Math.Pow(2, (exponentValue - 127));
+                        float floatValue = BitConverter.ToSingle(BitConverter.GetBytes(hexValue), 0);
+                        enableEvent = false;
+                        TextBoxFloatValue.Text = floatValue.ToString();
+                        enableEvent = true;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void TextBoxFloatValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (enableEvent)
+            {
+                try
+                {
+                    float floatValue = (float) Convert.ToDouble(TextBoxFloatValue.Text);
+                    UInt32 hexValue = BitConverter.ToUInt32(BitConverter.GetBytes(floatValue), 0);
+                    enableEvent = false;
+                    TextBoxHexValue.Text = String.Format("{0:X}", hexValue);
+                    enableEvent = true;
+                }
+                catch
+                {
+
+                }
+            }
+            e.Handled = true;
+        }
+    }
+}
